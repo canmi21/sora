b67313838d4b43d62ae503f1fcef4cb356e94d42
Canmi <9997200@qq.com>
2025-09-16 15:37:02 +0800

    chore: update next.config.ts to allow local dev origins and enable viewTransition

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "192.168.*.*", "172.*.*.*"],
+  experimental: {
+    viewTransition: true,
+  },
   /* config options here */
 };
 

2af1b2d110808f17c717529572075b36edf0eb59
Canmi <9997200@qq.com>
2025-09-16 15:37:03 +0800

    refactor: rename RootLayout to root_layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,17 @@ import "~/styles/tailwindcss.css";
 import "~/styles/transitions.css";
 import "~/styles/colors.css";
 import type { ReactNode } from "react";
+import { site } from "~/metadata/meta";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export const metadata = site;
+
+export default function root_layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />

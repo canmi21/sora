eba254077bff3bd5825007173d68a78559246841
Canmi <9997200@qq.com>
2025-09-17 08:48:09 +0800

    feat: add more allowed development origins

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "192.168.*.*", "172.*.*.*"],
+  allowedDevOrigins: [
+    "local-origin.dev",
+    "*.local-origin.dev",
+    "192.168.*.*",
+    "172.*.*.*",
+  ],
   experimental: {
     viewTransition: true,
   },

f106dd56a346432d5990728b324dd049c77b1239
Canmi <9997200@qq.com>
2025-09-26 23:43:14 +0800

    feat: structure AppProvider with ThemeProvider, ColorProvider, and AppLayout

--- src/contexts/app.tsx
+++ src/contexts/app.tsx
@@ -7,28 +7,28 @@ import { ColorProvider, use_color_context } from "./color";
 import { ThemeProvider } from "./theme";
 
 function AppLayout({ children }: { children: ReactNode }) {
-  const { theme_color } = use_color_context();
+	const { theme_color } = use_color_context();
 
-  return (
-    <div
-      className="color"
-      style={
-        {
-          "--theme-color": `var(--theme-color-${theme_color})`,
-        } as React.CSSProperties
-      }
-    >
-      {children}
-    </div>
-  );
+	return (
+		<div
+			className="color"
+			style={
+				{
+					"--theme-color": `var(--theme-color-${theme_color})`,
+				} as React.CSSProperties
+			}
+		>
+			{children}
+		</div>
+	);
 }
 
 export function AppProvider({ children }: { children: ReactNode }) {
-  return (
-    <ThemeProvider>
-      <ColorProvider>
-        <AppLayout>{children}</AppLayout>
-      </ColorProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<ColorProvider>
+				<AppLayout>{children}</AppLayout>
+			</ColorProvider>
+		</ThemeProvider>
+	);
 }

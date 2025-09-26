f301cfd3590661ba6d55a1e5379665f9fec1fb13
Canmi <9997200@qq.com>
2025-09-26 23:43:13 +0800

    feat: add initial empty page with FireFly component

--- src/app/(space)/page.tsx
+++ src/app/(space)/page.tsx
@@ -3,14 +3,14 @@
 import FireFly from "~/components/shared/fire-fly";
 
 export default function home() {
-  return (
-    <>
-      <FireFly maxDots={30} />
-      <div className="flex-grow flex items-center justify-center relative z-10">
-        <h1 className="text-4xl font-semibold text-[var(--color-text)]">
-          Empty
-        </h1>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<FireFly maxDots={30} />
+			<div className="flex-grow flex items-center justify-center relative z-10">
+				<h1 className="text-4xl font-semibold text-[var(--color-text)]">
+					Empty
+				</h1>
+			</div>
+		</>
+	);
 }

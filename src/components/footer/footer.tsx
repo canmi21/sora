70ad36c82bd33a63703650107af0cd3d514e0ef5
Canmi <9997200@qq.com>
2025-09-27 20:36:46 +0800

    feat: dynamically populate footer links using ValueContext

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,23 +5,36 @@ import { FooterNavigation } from "./navigation";
 import { FooterDynamicContent } from "./dynamic";
 import { IcpList } from "./icp";
 import { Copyright } from "./copyright";
+import { LinkGroup } from "./link-group";
 
 /**
- * The main footer component.
- * It fetches all required data and composes the layout by passing the dynamic
- * client components into their designated props on the static layout component.
+ * The main footer component. Fetches all data and assembles the layout.
  */
 export default function Footer() {
-	// The server retrieves the year to avoid errors in the wayback machine.
 	const current_year = new Date().getFullYear();
 
 	return (
 		<ValueProvider
-			keysToFetch={["owner.quote", "site.icp", "owner.name", "site.inception"]}
+			keysToFetch={[
+				"owner.quote",
+				"site.icp",
+				"owner.name",
+				"site.inception",
+				"site.link.me",
+				"site.link.site",
+				"site.link.project",
+				"site.link.repos",
+				"site.link.opensource",
+				"site.link.sponsor",
+				"site.link.git",
+				"site.link.email",
+				"site.link.status",
+			]}
 		>
 			<footer className="site-footer w-full border-t border-[var(--footer-border-color)] bg-[var(--footer-background)]">
 				<div className="max-w-7xl mx-auto px-18 relative py-6.5">
 					<FooterNavigation
+						linkGroupContent={<LinkGroup />}
 						copyrightContent={<Copyright currentYear={current_year} />}
 						dynamicContent={<FooterDynamicContent />}
 						icpContent={<IcpList />}
